e6e5d265b3ed379202f49f44c1bd9977b3af1542
westolt <william.stolt@helsinki.fi>
2026-01-06 16:55:49 +0200

    Handle unlocked rewards in RewardList and RewardButton

--- frontend/src/components/RewardButton.jsx
+++ frontend/src/components/RewardButton.jsx
@@ -1,18 +1,66 @@
-const RewardButton = ({ name, image, onClick, onMouseEnter, onMouseLeave }) => {
+import { useEffect, useState } from "react"
+
+const RewardButton = ({ name, image, onClick, onMouseEnter, onMouseLeave, usernameFont, setUsernameFont, gameFont, setGameFont, unlockedRewards }) => {
+  const [isUnlocked, setIsUnlocked] = useState(false)
+
+  useEffect(() => {
+    let unlocked = false
+    if (unlockedRewards) {
+      unlocked = unlockedRewards.find(r => r.rewardName === name)
+    }
+    setIsUnlocked(unlocked || false)
+  }, [name, unlockedRewards])
+
+
+  const handleChange = () => {
+    return null
+  }
 
   return (
-    <button
-      className="button reward-button"
-      onClick={onClick}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
+    <div className="selections">
+      <button
+        className="button reward-button"
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
       <img 
-      src={image}
-      alt={name}
+        src={image}
+        alt={name}
       />
       <span>{name}</span>
-    </button>
+      </button>
+      <div>
+        {isUnlocked ? (
+          <>
+          <div className="equip">
+            <div className="title">Username</div>
+            <input
+            type="checkbox"
+            id="username"
+            name="username"
+            value="username"
+            checked={usernameFont.username}
+            onChange={handleChange}
+            />
+          </div>
+          <div className="equip">
+            <div className="title">Game</div>
+            <input
+            type="checkbox"
+            id="gamefont"
+            name="gamefont"
+            value="gamefont"
+            checked={gameFont.gamefont}
+            onChange={handleChange}
+            />
+          </div>
+          </>
+        ) : (
+          <>Locked</>
+        )}
+      </div>
+  </div>
   )
 }
 
--- frontend/src/components/RewardList.jsx
+++ frontend/src/components/RewardList.jsx
@@ -1,21 +1,47 @@
 import { useState, useEffect } from 'react'
 import rewardService from '../services/rewards'
+import userService from '../services/users'
 import RewardButton from './RewardButton'
 import './rewardlist.css'
 
 const RewardList = ({ hoverChange }) => {
     const [rewards, setRewards] = useState([])
+    const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [usernameFont, setUsernameFont] = useState(false)
+    const [gameFont, setGameFont] = useState(false)
 
     useEffect(() => {
         rewardService.getAll().then(data => {
             setRewards(data)
             setLoading(false)
         })
+        const fetchUser = async () => {
+            const loggedUserJSON = window.localStorage.getItem('loggedUser')
+            if (loggedUserJSON) {
+                const loggedUser = JSON.parse(loggedUserJSON)
+                userService.setToken(loggedUser.token)
+
+                const fullUser = await userService.getOne(loggedUser.id)
+                setUser({
+                    ...fullUser,
+                    token: loggedUser.token
+                })
+            }
+        }
+        fetchUser()
     }, [])
 
+    const getUnlockedRewards = (user, rewards) => {
+        if (!user || !user.unlockedRewards) return []
+        const unlocked = user.unlockedRewards.map(r => r.rewardName)
+        return rewards.filter(reward => unlocked.includes(reward.rewardName))
+    }
+
+    const unlockedRewards = user ? getUnlockedRewards(user, rewards) : null
+
     const handleClick = () => {
-        return console.log('Clicked!')
+        return console.log('Unlocked Rewards: ', unlockedRewards)
     }
 
     if (loading) return <div className='loading'>Loading rewards...</div>
@@ -31,6 +57,11 @@ const RewardList = ({ hoverChange }) => {
                         onClick={() => handleClick(reward.id)}
                         onMouseEnter={()=>hoverChange(reward)}
                         onMouseLeave={()=>hoverChange(null)}
+                        usernameFont={usernameFont}
+                        setUsernameFont={setUsernameFont}
+                        setGameFont={setGameFont}
+                        gameFont={gameFont}
+                        unlockedRewards={unlockedRewards}
                             />
                 )}
             </div>
